dab2f9c3992dcbde7ae2a69454e8e16b3107e3d5
jamessam <jamessam@users.noreply.github.com>
2018-05-05 10:07:46 -0700

    python proof of concept is done

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import contentful as ctf
 import contentful_management as mgnt
 import json
 import os
-import shutil
+import requests
 import subprocess
 import sys
 
@@ -10,12 +10,35 @@ import sys
 SPACE_ID = os.environ['SPACE_ID']
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 MGNT_TOKEN = os.environ['MGNT_TOKEN']
+ENVIRONMENT_ID = 'master'
 
 
 def copy_jpg(asset):
     url = asset.file['url']
     base = url.split('/')[-1]
-    # shutil.copy('https:'+url, base)
+    r = requests.get('https:'+url)
+    with open(base, 'wb') as local:
+        local.write(r.content)
+
+
+def embed_metdata(read_only_entry, keywords):
+    entry = mgnt_client.entries(SPACE_ID, ENVIRONMENT_ID).find(
+        read_only_entry.id)
+    content_type = entry.sys['content_type'].resolve(
+        space_id=SPACE_ID, environment_id=ENVIRONMENT_ID)
+    for field in content_type.fields:
+        if field.id != 'keywords':
+            continue
+        for keyword in keywords:
+            # Skip words already in there.
+            try:
+                if keyword in entry.fields()['keywords']:
+                    continue
+            except:
+                entry.fields()['keywords'] = []
+            entry.fields()['keywords'].append(keyword)
+        entry.save()
+        entry.publish()
 
 
 def get_jpg_assets(client):
@@ -35,8 +58,6 @@ def run_exif_command(command):
 
 
 def main():
-    delivery_client = ctf.Client(SPACE_ID, ACCESS_TOKEN, environment='master')
-
     # Find all the jpg/jpeg assets
     jpg_assets = get_jpg_assets(delivery_client)
 
@@ -44,10 +65,8 @@ def main():
     for asset in jpg_assets:
         # See if there's a linked entry.
         # If there isn't, proceed to the next asset.
-        # TODO: get the entries from the management client
         linked_entries = delivery_client.entries(
-            {'links_to_asset': asset.sys['id']}
-        )
+            {'links_to_asset': asset.sys['id']})
         if len(linked_entries) < 1:
             continue
 
@@ -69,18 +88,11 @@ def main():
 
         # In each linked entry, see if there's a subject field in the entry
         # for the keyword data to go into.
-        for entry in linked_entries:
-            content_type = entry.sys['content_type'].resolve(delivery_client)
-            for field in content_type.fields:
-                if field.id != 'keywords':
-                    continue
-                for keyword in keywords:
-                    # Skip words already in there.
-                    if keyword in entry.fields()['keywords']:
-                        continue
-                    entry.fields()['keywords'].append(keyword)
-                entry.save()
+        for read_only_entry in linked_entries:
+            embed_metdata(read_only_entry, keywords)
 
 
 if __name__ == '__main__':
+    delivery_client = ctf.Client(SPACE_ID, ACCESS_TOKEN, environment=ENVIRONMENT_ID)
+    mgnt_client = mgnt.Client(MGNT_TOKEN)
     main()
