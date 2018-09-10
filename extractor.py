9b2b9df4fc61a1c8d093da8587c92b0800ad90a4
Jim Sam <jamessam@users.noreply.github.com>
2018-09-09 22:30:31 -0700

    refactor for custom payloads

--- extractor.py
+++ extractor.py
@@ -1,25 +1,31 @@
 import contentful as ctf
 import contentful_management as mgnt
-import datetime
-from flask import Flask, request
-import json
 import os
-import pytz
 import requests
-import subprocess
 import sys
 
 from PIL import Image, IptcImagePlugin
 from PIL.ExifTags import TAGS
 
 
-delivery_client = ctf.Client(
+# CONSTANTS
+cda = ctf.Client(
     os.environ['SPACE_ID'],
     os.environ['ACCESS_TOKEN'],
     environment='master')
-mgnt_client = mgnt.Client(os.environ['MGNT_TOKEN'])
+cma = mgnt.Client(os.environ['MGNT'])
 
-app = Flask(__name__)
+
+# FUNCTIONS
+def check_for_keywords(entry):
+    '''Examines the content model of a Contentful content entry for a field
+    called "keywords", assumed to be a short text list.'''
+    content_type = cma.content_types(os.environ['SPACE_ID'],'master').find(
+        entry.sys['content_type'].id)
+    for field in content_type.fields:
+        if field.id == 'keywords':
+            return True
+    return False
 
 
 def copy_jpg(asset):
@@ -44,36 +50,9 @@ def get_keywords(file_name):
     return keywords
 
 
-def get_recent_assets():
-    '''Filters a list of assets to just what was published the day this function
-    is invoked.'''
-    now = pytz.utc.localize(datetime.datetime.utcnow())
-    yesterday = now - datetime.timedelta(days=1)
-
-    todays_assets = []
-    for asset in delivery_client.assets():
-        if not yesterday < asset.sys['updated_at'] <= now:
-            continue
-        todays_assets.append(asset)
-
-    return todays_assets
-
-
-def keywords_in_content_model(entry):
-    '''Examines the content model of a Contentful content entry for a field
-    called "keywords", assumed to be a short text list.'''
-    content_type = mgnt_client.content_types(
-        os.environ['SPACE_ID'],
-        'master').find(entry.sys['content_type'].id)
-    for field in content_type.fields:
-        if field.id == 'keywords':
-            return True
-    return False
-
-
 def write_metadata(read_only_entry, keywords):
     '''Writes the IPTC keywords to the contentful-management API. Returns nothing.'''
-    entry = mgnt_client.entries(os.environ['SPACE_ID'], 'master').find(
+    entry = cma.entries(os.environ['SPACE_ID'], 'master').find(
         read_only_entry.id)
     for keyword in keywords:
         try:
@@ -85,40 +64,34 @@ def write_metadata(read_only_entry, keywords):
     entry.publish()
 
 
-@app.route("/", methods=('POST',))
-def main():
-    if request.method != 'POST':
-        return None
+def main(assetId):
+    print(f'Starting extractor for {assetId}')
+    # See if there's a linked entry. If there isn't, exit.
+    linked_entries = cda.entries({'links_to_asset': assetId})
+    if len(linked_entries.items) < 1:
+        sys.exit(f'There are no linked entries for asset {assetId}.')
 
-    assets = get_recent_assets()
-    for asset in assets:
-        ASSET_ID = asset.sys['id']
+    # Filter out the entries with no keywords field on its content type.
+    filtered_entries = [entry for entry in linked_entries if check_for_keywords(entry)]
+    if len(filtered_entries) == 0:
+        sys.exit(f'There are no entries with a keywords field attached to {assetId}')
 
-        # See if there's a linked entry. If there isn't, exit.
-        linked_entries = delivery_client.entries({'links_to_asset': ASSET_ID})
-        if len(linked_entries) < 1:
-            sys.exit(f'There are no linked entries for asset {ASSET_ID}.')
+    # Load the asset object
+    asset = cda.asset(assetId)
 
-        # Get the keyword metadata for each file. If no keyword, exit.
-        # Regardless, remove the file.
-        for read_only_entry in linked_entries:
-            # Look for a keywords field in the linked entry.
-            if not keywords_in_content_model(read_only_entry):
-                sys.exit(f'There\'s no keywords field in the content model of {read_only_entry}')
+    # Copy the file from Contentful to the server
+    copy_jpg(asset)
 
-            # If so, copy the file from Contentful to the server
-            asset = delivery_client.asset(ASSET_ID)
-            copy_jpg(asset)
+    # Extract the keywords and write them to the appropriate records
+    try:
+        file_name = f"/tmp/{asset.fields()['file']['fileName']}"
+        keywords = get_keywords(file_name)
+        [write_metadata(entry, keywords) for entry in filtered_entries]
+    except KeyError as ke:
+        print(f'There was a problem writing metadata to the database: {ke}')
 
-            try:
-                file_name = f"/tmp/{asset.fields()['file']['fileName']}"
-                keywords = get_keywords(file_name)
-                write_metadata(read_only_entry, keywords)
-            except KeyError as ke:
-                print(f'There was a problem writing metadata to the database: {ke}')
-
-    return str(assets)
+    return None
 
 
 if __name__ == '__main__':
-    app.run()
+    main(sys.argv[1])
